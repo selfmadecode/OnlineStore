23b1e6c50047b8c7743c53c09a3444f488818f3c
selfmadecode <anyanwuraphaelc@gmail.com>
2020-09-29 00:40:55 +0100

    Admin can add store manager role

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using SmartStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SmartStore.Controllers
+{
+    [Authorize(Roles = RoleName.Admin)]
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult AddStoreManager()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddManager(ApplicationDbContext context, StoreManager storeManager)
+        {
+            if (ModelState.IsValid)
+            {
+                if (!context.Users.Any(u => u.UserName == storeManager.EmailAddress))
+                {
+                    var user = new UserStore<ApplicationUser>(context);
+                    var userManager = new UserManager<ApplicationUser>(user);
+
+                    var passwordHasher = new PasswordHasher();
+                    var admin = new ApplicationUser
+                    {
+                        UserName = storeManager.EmailAddress,
+                        PasswordHash = passwordHasher.HashPassword(storeManager.Password)
+                    };
+                    userManager.Create(admin);
+                    userManager.AddToRole(admin.Id, RoleName.StoreManager);
+                    ViewBag.NewManager = "Store Manager Added succesffully";
+                }
+            }
+            return RedirectToAction("Products", "Shop");
+            
+        }
+    }
+}
